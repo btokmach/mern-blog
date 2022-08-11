3a528559f469941d071dba5c11d329be0a79d36d
btokmach <btokmach@gmail.com>
2022-08-11 23:25:20 +0300

    setup express and mongo

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+const app = express()
+dotenv.config()
+
+
+const PORT = process.env.PORT || 3001
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+
+
+app.use(cors())
+app.use(express.json())
+
+
+async function start() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.rjargeu.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+    )
+
+    app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
