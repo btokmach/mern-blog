0d39e12df7273a0c8f84d97606eb632e7fb50ac6
btokmach <btokmach@gmail.com>
2022-08-13 22:25:19 +0300

    setup react and tailwind

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="text-xl text-red-600">
+      Connect
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/AddPostPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddPostPage = () => {
+  return <div>AddPostPage</div>;
+};
+
+export default AddPostPage;
