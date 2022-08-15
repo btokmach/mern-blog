a2854f947ce0b15ab57bbd9666e369ec3271fc76
btokmach <btokmach@gmail.com>
2022-08-15 18:48:44 +0300

    user registration completed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -23,6 +23,14 @@ export const registration = async (req, res) => {
       password: hash,
     });
 
+    const token = jwt.sign(
+      {
+        id: newUser._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+
     await newUser.save();
 
     res.json({
